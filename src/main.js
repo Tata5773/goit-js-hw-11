f21218982bc770e5c73e086423f270f928f02ef4
Tata5773 <124725617+Tata5773@users.noreply.github.com>
2026-01-17 23:37:59 +0100

    homework 11

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  clearGallery,
+  createGallery,
+  hideLoader,
+  showLoader,
+} from './js/render-functions';
+import './css/styles.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const query = event.target.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search query',
+    });
+    return;
+  }
+  console.log(query);
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(() => {
+      console.log(error);
+    })
+    .finally(() => {
+      hideLoader();
+      form.reset();
+    });
+});
